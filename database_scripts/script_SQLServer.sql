d80c3831ae7e776207c9d663f014a0eb657d0014
Danny R. Moreno <108888740+DannyRMoreno28@users.noreply.github.com>
2023-07-10 19:01:01 -0500

    Update script_SQLServer.sql

--- database_scripts/script_SQLServer.sql
+++ database_scripts/script_SQLServer.sql
@@ -1,47 +1,53 @@
+-- Crear la base de datos "organizador_peliculas"
 USE [master]
 GO
 CREATE DATABASE [organizador_peliculas]
     CONTAINMENT = NONE
-    ON  PRIMARY 
-( NAME = N'organizador_peliculas', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\organizador_peliculas.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+    ON PRIMARY 
+    ( NAME = N'organizador_peliculas', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\organizador_peliculas.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
     LOG ON 
-( NAME = N'organizador_peliculas_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\organizador_peliculas_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+    ( NAME = N'organizador_peliculas_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\organizador_peliculas_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
     WITH CATALOG_COLLATION = DATABASE_DEFAULT
 GO
 
+-- Cambiar al contexto de la base de datos "organizador_peliculas"
 USE [organizador_peliculas]
 GO
 
+-- Crear la tabla "genero"
 CREATE TABLE [dbo].[genero](
-	[id_genero] [int] NOT NULL,
-	[titulo] [varchar](255) NULL,
-	PRIMARY KEY CLUSTERED 
-	(
-		[id_genero] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+    [id_genero] [int] NOT NULL,
+    [titulo] [varchar](255) NULL,
+    PRIMARY KEY CLUSTERED 
+    (
+        [id_genero] ASC
+    ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
+-- Crear la tabla "genero_pelicula"
 CREATE TABLE [dbo].[genero_pelicula](
-	[id_pelicula] [int] NOT NULL,
-	[id_genero] [int] NOT NULL
+    [id_pelicula] [int] NOT NULL,
+    [id_genero] [int] NOT NULL
 ) ON [PRIMARY]
 GO
 
+-- Crear la tabla "pelicula"
 CREATE TABLE [dbo].[pelicula](
-	[id_pelicula] [int] IDENTITY(1,1) NOT NULL,
-	[fecha_estreno] [date] NULL,
-	[ruta_portada] [varchar](255) NULL,
-	[sinopsis] [varchar](500) NULL,
-	[titulo] [varchar](255) NULL,
-	[youtube_trailer_id] [varchar](255) NULL,
-	PRIMARY KEY CLUSTERED 
-	(
-		[id_pelicula] ASC
-	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+    [id_pelicula] [int] IDENTITY(1,1) NOT NULL,
+    [fecha_estreno] [date] NULL,
+    [ruta_portada] [varchar](255) NULL,
+    [sinopsis] [varchar](500) NULL,
+    [titulo] [varchar](255) NULL,
+    [youtube_trailer_id] [varchar](255) NULL,
+    PRIMARY KEY CLUSTERED 
+    (
+        [id_pelicula] ASC
+    ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
+-- Agregar restricciones de clave foránea a la tabla "genero_pelicula"
 ALTER TABLE [dbo].[genero_pelicula]  WITH CHECK ADD  CONSTRAINT [FK7rxwafj1id7f40i0mq446ivw6] FOREIGN KEY([id_pelicula])
 REFERENCES [dbo].[pelicula] ([id_pelicula])
 GO
@@ -56,5 +62,6 @@ GO
 ALTER TABLE [dbo].[genero_pelicula] CHECK CONSTRAINT [FKnpb8vdeybu4p41dibnbawra2q]
 GO
 
-ALTER DATABASE [organizador_peliculas] SET  READ_WRITE 
+-- Establecer la base de datos "organizador_peliculas" en modo lectura y escritura
+ALTER DATABASE [organizador_peliculas] SET READ_WRITE 
 GO
